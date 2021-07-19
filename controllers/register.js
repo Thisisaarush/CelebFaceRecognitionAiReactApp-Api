02050cdc33f14f84b9ac4849a3ed8884e7485fb0
Aarush Tanwar <tanwaraarush007@gmail.com>
2021-07-19 20:58:48 +0530

    fixing unknown errors lol

--- controllers/register.js
+++ controllers/register.js
@@ -6,32 +6,30 @@ const handleRegister = (req, res, db, bcrypt) => {
   }
 
   const hash = bcrypt.hashSync(password);
-  db.transaction(trx => {
-    trx.insert({
-      hash: hash,
-      email: email
-    })
-    .into('login')
-    .returning('email')
-    .then(loginEmail => {
-      return trx('users')
-      .returning('*')
-      .insert({
-        email: loginEmail[0],
-        name: name,
-        joined: new Date()
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
       })
-      .then(user => {
-        res.json(user[0]);
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
       })
+      .then(trx.commit)
+      .catch(trx.rollback)
     })
-    .then(trx.commit)
-    .catch(trx.rollback)
-  })
-
-  .catch(err => res.status(400).json('Unable to register'));
-  
-};
+    .catch(err => res.status(400).json('unable to register'))
+}
 
 module.exports = {
   handleRegister: handleRegister
